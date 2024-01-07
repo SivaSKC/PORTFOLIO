21bddc9cf3288ef3d41a8c0cede655a1b60ae90a
SivaSKC <sivask4325@gmail.com>
2024-01-07 23:47:14 +0530

    update

--- image/portfolio.php
+++ image/portfolio.php
@@ -3,7 +3,7 @@ $a=$_POST['mobileno'];
 $b=$_POST['email'];
 $c=$_POST['message'];
 $con=mysqli_connect("localhost","root","","enquiry");
-$sql="INSERT INTO cst_details(Cst_No,Cst_email,Cst_Message) values ($a,$b,$c)";
+$sql="INSERT INTO cst_details(Cst_No,Cst_email,Cst_Message) values ('$a','$b','$c')";
 $d=mysqli_query($con,$sql);
 if($d)
 {
