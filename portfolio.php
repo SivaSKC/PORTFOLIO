754390f8685e212055d8c43d30285de4383940f6
SivaSKC <sivask4325@gmail.com>
2024-01-07 22:45:47 +0530

    update

---
+++ portfolio.php
@@ -0,0 +1,15 @@
+<?php
+$a=$_POST['mobileno'];
+$b=$_POST['email'];
+$c=$_POST['message'];
+$con=mysqli_connect("localhost","root","","enquiry");
+$sql="INSERT INTO UG(Cst_No,Cst_email,Cst_Message) values ($a,$b,$c)";
+$d=mysqli_query($con,$sql);
+if($d)
+{
+    echo "Successfully Submit";
+}
+else
+{
+    echo "Not Added";
+}
